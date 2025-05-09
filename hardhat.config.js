c10b4346ab57f9501a314562cfb9fcc372d502a6
Dilmurod <luxdesignuz@gmail.com>
2025-05-09 21:26:15 +0500

    Update gitignore, hardhat config and deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-require("dotenv").config({ path: './config/.env' });
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     usochain: {
-      url: "http://127.0.0.1:8545",
+      url: process.env.RPC_URL,
       chainId: 571,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
