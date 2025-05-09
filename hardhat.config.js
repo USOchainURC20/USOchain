7ce15abfc7f924a5d1acaa9549952a7d11b75f6a
Dilmurod <luxdesignuz@gmail.com>
2025-05-09 19:52:16 +0500

    Your commit message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
-require("dotenv").config(); // Load environment variables from the .env file
-require("@nomicfoundation/hardhat-toolbox"); // Hardhat toolbox which includes useful plugins
+require("dotenv").config({ path: './config/.env' });
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     usochain: {
-      url: "http://127.0.0.1:8545", // RPC URL
-      chainId: 571, // Chain ID
-      accounts: [process.env.PRIVATE_KEY], // Private key from .env
+      url: "http://127.0.0.1:8545",
+      chainId: 571,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,41 +3,32 @@ const hre = require("hardhat"); // Hardhat Runtime Environment
 
 async function main() {
   try {
-    // Get the deployer's wallet
     const [deployer] = await hre.ethers.getSigners();
 
     console.log("Deploying contracts with the account:", deployer.address);
 
-    // Get and display deployer's balance
     const balance = await deployer.getBalance();
     console.log("Account balance:", hre.ethers.utils.formatEther(balance));
 
-    // Get the contract factory for URC20
     const USOD = await hre.ethers.getContractFactory("URC20");
 
-    // Define total supply (571 octodecillion with 6 decimals)
     const totalSupply = hre.ethers.BigNumber.from("571000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
 
-    // Deploy the contract
     const usod = await USOD.deploy(
-      "Universal Stablecoin Original Dollar", // Token name
-      "USOD",                                 // Token symbol
-      6,                                      // Decimals
-      totalSupply                             // Initial supply
+      "Universal Stablecoin Original Dollar",
+      "USOD",                                 
+      6,                                      
+      totalSupply                             
     );
 
-    // Wait for the contract to be deployed
     await usod.deployed();
 
-    // If contract deployment is successful, print contract address
     console.log("✅ USOD deployed to:", usod.address);
 
   } catch (error) {
-    // Catch and print any errors during deployment
     console.error("❌ Error deploying:", error);
     process.exitCode = 1;
   }
 }
 
-// Run the main function
 main();
