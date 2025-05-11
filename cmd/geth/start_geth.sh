059128a63d2cc17422d2d49d2a5df50d5df2afca
Dilmurod <luxdesignuz@gmail.com>
2025-05-11 21:16:23 +0500

    Initial commit

--- cmd/geth/start_geth.sh
+++ cmd/geth/start_geth.sh
@@ -4,10 +4,15 @@ geth --datadir ~/g/GethData \
   --http \
   --http.addr "0.0.0.0" \
   --http.port 8545 \
-  --http.api "db,eth,net,web3,personal" \
+  --http.api "db,eth,net,web3,personal,miner" \
   --mine \
   --miner.etherbase 0xdd6342cD45CE437382C00523cc6F22968bdd21b8 \
   --http.corsdomain "*" \
   --ipcpath ~/g/GethData/geth.ipc \
-  --http.vhosts "usochain.urc" \
-  --port 30303
+  --http.vhosts "*" \
+  --port 30303 \
+  --allow-insecure-unlock \
+  --nodiscover \
+  --verbosity 3 \
+  --syncmode "full" \
+  --gcmode "archive"
